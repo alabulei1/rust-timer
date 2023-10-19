71a01206e785d3adc492cf9187dc867756f9589b
alabulei1 <vivian.xiage@gmail.com>
2023-10-19 17:47:28 +0800

    Add files via upload

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use schedule_flows::{schedule_cron_job, schedule_handler};
+use slack_flows::send_message_to_channel;
+
+#[no_mangle]
+#[tokio::main(flavor = "current_thread")]
+pub async fn on_deploy() {
+    // Note: the cron syntax uses UTC time.
+    // Adjust the time according to your desired timezone.
+    schedule_cron_job(String::from("0 7 30 10 *"), String::from("Please remember to shut up the AWS machine before 2023/10/31.")).await;
+}
+
+#[schedule_handler]
+async fn handler(body: Vec<u8>) {
+    send_message_to_channel(
+        "secondstate",
+        "ai",
+        String::from_utf8_lossy(&body).into_owned(),
+    ).await;
+}
